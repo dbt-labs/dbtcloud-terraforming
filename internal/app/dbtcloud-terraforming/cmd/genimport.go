bca32427584cfdc9964db7477d7d0482e3316cf4
Benoit Perigaud <8754100+b-per@users.noreply.github.com>
2025-02-05 22:16:28 +0100

    Add genimport command

---
+++ internal/app/dbtcloud-terraforming/cmd/genimport.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(genimportCmd)
+}
+
+var genimportCmd = &cobra.Command{
+	Use:    "genimport",
+	Short:  "Generate Terraform configuration and import commands for dbt Cloud resources",
+	Long:   "Combines the functionality of 'generate' and 'import' commands to create Terraform configuration and corresponding import commands in one step",
+	Run:    runGenImport(),
+	PreRun: sharedPreRun,
+}
+
+func runGenImport() func(cmd *cobra.Command, args []string) {
+	return func(cmd *cobra.Command, args []string) {
+		// First run generate
+		generateResources()(cmd, args)
+
+		// Then run import
+		runImport()(cmd, args)
+	}
+}
